b7111ffb3109fce2cca5d7015c902437b8bbbece
laloe74 <bernardlaloe74@gmail.com>
2024-07-05 22:25:11 +0800

    fix: 忽略.mp4格式

--- v2mkv.py
+++ v2mkv.py
@@ -1,9 +1,7 @@
-# 将桌面from文件夹里的所有视频转换成mp4格式存放到桌面to文件夹里
-
-
 import os
 import subprocess
 import re
+import shutil
 import time
 
 # FFmpeg支持的视频格式
@@ -15,6 +13,10 @@ supported_formats = [
 ]
 
 log_file_path = os.path.expanduser('~/Desktop/conversion_log.txt')
+
+def create_log_file():
+    with open(log_file_path, 'w') as log_file:
+        log_file.write("")
 
 def count_supported_videos(folder):
     count = 0
@@ -41,35 +43,36 @@ def read_log():
                 processed_files.add(line.strip())
     return processed_files
 
-def write_log(file_name):
+def write_log_section(title, content_list):
     with open(log_file_path, 'a') as log_file:
-        log_file.write(f"{file_name}\n")
+        log_file.write(f"\n## {title}\n")
+        for content in content_list:
+            log_file.write(f"{content}\n")
 
-def write_summary(expected_count, success_count, fail_count, total_time, unsupported_files, failed_files, success_files):
+def write_summary(expected_count, success_count, fail_count, total_time, unsupported_files, failed_files, success_files, no_processing_files):
     with open(log_file_path, 'a') as log_file:
         log_file.write("\n## 基本信息\n")
         log_file.write(f"本次运行预计处理数：{expected_count}\n")
         log_file.write(f"本次运行成功次数：{success_count}\n")
         log_file.write(f"本次运行失败次数：{fail_count}\n")
         log_file.write(f"本次运行总时间：{total_time:.2f}秒\n")
-        log_file.write("\n## 错误信息\n")
-        for file in unsupported_files:
-            log_file.write(f"不支持的文件：{file}\n")
-        for file in failed_files:
-            log_file.write(f"未处理的文件：{file}\n")
-        log_file.write("\n## 成功信息\n")
-        for file in success_files:
-            log_file.write(f"{file}\n")
+    write_log_section("错误信息", [f"不支持的文件：{file}" for file in unsupported_files])
+    write_log_section("错误信息", [f"未处理的文件：{file}" for file in failed_files])
+    write_log_section("成功信息", success_files)
+    write_log_section("无须处理", no_processing_files)
 
 def convert_videos(from_folder, to_folder):
+    create_log_file()
+
     if not os.path.exists(to_folder):
         os.makedirs(to_folder)
 
     total_files = count_supported_videos(from_folder)
     processed_files = 0
-    failed_files = 0
+    error_files = 0
     unsupported_files = []
     failed_files_list = []
+    no_processing_files = []
     processed_set = read_log()
     success_files = list(processed_set)
     success_count = 0
@@ -81,23 +84,32 @@ def convert_videos(from_folder, to_folder):
 
     for root, dirs, files in os.walk(from_folder):
         for file in files:
+            processed_files += 1
             file_extension = os.path.splitext(file)[1].lower()
             
             if file == '.DS_Store':
                 continue
             
+            input_path = os.path.join(root, file)
+            relative_path = os.path.relpath(root, from_folder)
+            output_dir = os.path.join(to_folder, relative_path)
+            os.makedirs(output_dir, exist_ok=True)
+            
+            output_file = os.path.splitext(file)[0] + '.mp4'
+            output_path = os.path.join(output_dir, output_file)
+            
+            if file_extension == '.mp4':
+                shutil.copy2(input_path, output_path)
+                no_processing_files.append(file)
+                print(f"\r正在处理: {processed_files}/{total_files}/{error_files} -- 跳过MP4文件: {file}")
+                write_log_section("无须处理", [file])
+                continue
+            
             if file_extension in supported_formats:
-                input_path = os.path.join(root, file)
-                relative_path = os.path.relpath(root, from_folder)
-                output_dir = os.path.join(to_folder, relative_path)
-                os.makedirs(output_dir, exist_ok=True)
-                
-                output_file = os.path.splitext(file)[0] + '.mp4'
-                output_path = os.path.join(output_dir, output_file)
-                
                 if file in processed_set:
-                    print(f"跳过已处理文件: {file}")
-                    processed_files += 1
+                    no_processing_files.append(file)
+                    print(f"\r正在处理: {processed_files}/{total_files}/{error_files} -- 跳过已处理文件: {file}")
+                    write_log_section("无须处理", [file])
                     continue
                 
                 try:
@@ -126,40 +138,41 @@ def convert_videos(from_folder, to_folder):
                                     progress = (time_in_seconds / duration) * 100
                                     progress_bar = '|' + '█' * int(progress // 2) + ' ' * (50 - int(progress // 2)) + '|'
                                     elapsed_time = time.time() - start_time
-                                    print(f"\r正在处理: {processed_files + 1}/{total_files}/{failed_files} {int(progress)}% {progress_bar} -- {file} ({elapsed_time:.2f}s)", end='')
+                                    print(f"\r正在处理: {processed_files}/{total_files}/{error_files} {int(progress)}% {progress_bar} -- {file} ({elapsed_time:.2f}s)", end='')
                     
                     process.wait()
                     if process.returncode == 0:
                         success_files.append(file)
-                        write_log(file)
+                        write_log_section("成功信息", [file])
                         success_count += 1
                     else:
-                        failed_files += 1
-                        fail_count += 1
                         failed_files_list.append(file)
+                        fail_count += 1
                         print(f"\n转换失败: {input_path}")
+                        write_log_section("错误信息", [f"未处理的文件：{file}"])
                     
                 except subprocess.CalledProcessError as e:
-                    failed_files += 1
-                    fail_count += 1
                     failed_files_list.append(file)
+                    fail_count += 1
                     print(f"\n转换失败: {input_path}")
                     print(e.stderr.decode())
+                    write_log_section("错误信息", [f"未处理的文件：{file}"])
                 
-                processed_files += 1
                 print()  # 换行以显示下一个文件的进度
             else:
                 unsupported_files.append(file)
-                print(f"文件格式不受支持: {file_extension} 文件: {file}")
+                error_files += 1
+                print(f"\r正在处理: {processed_files}/{total_files}/{error_files} -- 文件格式不受支持: {file_extension} 文件: {file}")
+                write_log_section("错误信息", [f"不支持的文件：{file}"])
 
     end_time = time.time()
     total_time = end_time - start_time
-    write_summary(total_files, success_count, fail_count, total_time, unsupported_files, failed_files_list, success_files)
+    write_summary(total_files, success_count, fail_count, total_time, unsupported_files, failed_files_list, success_files, no_processing_files)
 
     print("\n## 基本信息")
     print(f"本次运行预计处理数：{total_files}")
     print(f"本次运行成功次数：{success_count}")
-    print(f"本次运行失败次数：{failed_files}")
+    print(f"本次运行失败次数：{fail_count}")
     print(f"本次运行总时间：{total_time:.2f}秒")
 
 from_folder = os.path.expanduser('~/Desktop/from')
